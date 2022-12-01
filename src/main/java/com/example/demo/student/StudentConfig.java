72f81d8b946186dc973225110b7ffa438901338a
johnmuchiriM <jeanfrancaiseharicot@gmail.com>
2022-12-01 09:10:01 +0300

    update:post details as client

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,46 @@
+package com.example.demo.student;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam@jamal@gmail.com",
+                    LocalDate.of(2000, JANUARY,5)
+
+
+            );
+            Student kamau = new Student(
+                    "Kamau",
+                    "kamau@jamal@gmail.com",
+                    LocalDate.of(2002, JANUARY,5)
+
+
+            );
+            Student paul = new Student(
+                    "Paul",
+                    "paul@jamal@gmail.com",
+                    LocalDate.of(2004, JANUARY,5)
+
+
+            );
+
+            repository.saveAll(
+                    List.of(mariam,kamau,paul)
+            );
+        };
+    }
+}
